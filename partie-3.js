d076dce18532af66136e3f292394b27751f411ee
viki53 <viki53000@gmail.com>
2014-07-09 23:00:29 +0200

    Début partie 3 : décor

--- partie-3.js
+++ partie-3.js
@@ -4,7 +4,14 @@
  *                        *
  * ---------------------- */
 
+// Attention à ne pas utiliser cette méthode de chargement dans votre application, mais à inclure le fichier à la liste de la partie 2
+Q.load(['nuages.png'], function() {
+	console.log('Image de fond chargée');
+});
+
 
 Q.scene('game', function(stage) {
-	
+	console.log('Niveau 1 !');
+
+	stage.insert(new Q.Repeater({ asset: 'nuages.png', speedY: 0.8 }));
 });
