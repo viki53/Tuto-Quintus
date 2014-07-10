980fc762691b697a5511b5e2f33fa2fe7e15683f
viki53 <viki53000@gmail.com>
2014-07-11 00:09:21 +0200

    Fin de la partie 3

--- partie-3.js
+++ partie-3.js
@@ -5,18 +5,26 @@
  * ---------------------- */
 
 // Attention à ne pas utiliser cette méthode de chargement dans votre application, mais à inclure le fichier à la liste de la partie 2
-Q.load(['clouds.png', 'game-tiles.png', 'game.json', 'player-sprite.png', 'sheepfold-sprite.png'], function () {
+Q.load(['clouds.png', 'game-tiles.png', 'game.json', 'player-sprite.png', 'sheepfold-sprite.png', 'wolf-sprite.png'], function () {
 	console.log('Fichiers du niveau chargés');
 
 	Q.sheet('my_tiles', 'game-tiles.png', { tileW: 30, tileH: 30 }); // On crée des tiles
 	Q.sheet('my_player', 'player-sprite.png', { tileW: 25, tileH: 30 }); // On crée la feuille du joueur, qui permet de décomposer les états (pour l'animer par exemple)
-	Q.sheet('my_sheepfold', 'sheepfold-sprite.png', { tileW: 30, tileH: 30 }); // On crée la feuille du joueur, qui permet de décomposer les états (pour l'animer par exemple)
+	Q.sheet('my_sheepfold', 'sheepfold-sprite.png', { tileW: 30, tileH: 30 });
+	Q.sheet('my_wolf', 'wolf-sprite.png', { tileW: 30, tileH: 23 });
 
 	Q.animations('my_player', {
 		stand: { frames: [0], rate: 1/60, loop: true },
 		walk_left: { frames: [1], rate: 1/60 },
 		walk_right: { frames: [2], rate: 1/60 },
 		jump: { frames: [3], rate: 1/60 },
+	});
+
+	Q.animations('my_wolf', {
+		stand: { frames: [0], rate: 1/60, flip: false, loop: true },
+		walk_left: { frames: [1], rate: 1/60, flip: false, loop: true },
+		walk_right: { frames: [1], rate: 1/60, flip: 'x' }, // Eh oui, pour réduire le nombre d'images, il suffit de retourner celle que l'on souhaite !
+		dying: { frames: [1], rate: 1/60, flip: 'y' }, // Et ça fonctionne aussi pour une symétrie verticale !
 	});
 });
 
@@ -25,7 +33,7 @@ Q.Sprite.extend('Player', {
 		this._super(p, {
 			sheet: 'my_player',
 			sprite: 'my_player',
-			type: Q.SPRITE_PLAYER,
+			collisionMask: Q.SPRITE_DEFAULT,
 			speed: 300,
 			jumpSpeed: -500,
 			direction: null
@@ -80,6 +88,62 @@ Q.Sprite.extend('Sheepfold', {
 	}
 });
 
+Q.Sprite.extend('Wolf', {
+	init: function(p) {
+		this._super(p, {
+			sheet: 'my_wolf',
+			sprite: 'my_wolf',
+			vx: -100, // On part vers la gauche
+			collisionMask: Q.SPRITE_DEFAULT
+		});
+
+		this.add('2d, animation, aiBounce');
+
+		this.on('bump.left, bump.right, bump.bottom', function(collision) {
+			if(collision.obj.isA('Player')) {
+				Q.stageScene('endGame', 1, { label: 'Perdu !' });
+				collision.obj.destroy();
+			}
+		});
+
+		this.on('bump.top', this, 'die');
+	},
+	step: function (dt) {
+		if (this.p.x <= 0 || this.p.x >= Q.width) {
+			this.p.vx = -this.p.vx; // S'il va au bord, on inverse sa vitesse pour qu'il fasse demi-tour
+		}
+
+		if (this.p.vx > 0) {
+			this.p.direction = 'right';
+		}
+		else if (this.p.vx < 0) {
+			this.p.direction = 'left';
+		}
+		else {
+			this.p.direction = null;
+		}
+
+		if (this.p.direction) {
+			this.play('walk_' + this.p.direction);
+		}
+		else {
+			this.play('stand');
+		}
+	},
+	die: function (collision) {
+		this.p.vx = this.p.vy = 0; // Pas bouger !
+		this.play('dying'); // Fais le mort
+
+		(function (wolf) {
+			setTimeout(function() {
+				wolf.destroy(); // On attend un peu puis on le détruit (il ne gênera pas les autres loups ou le joueur, comme ça)
+			}, 300);
+		})(this);
+
+		collision.obj.p.vy = -300; // On fait rebondir le joueur
+	}
+});
+
 Q.scene('game', function (stage) {
 	console.log('Niveau 1 !');
 
@@ -105,6 +169,12 @@ Q.scene('game', function (stage) {
 		x: tiles.p.w - Q.sheets['my_sheepfold'].tileW/2,
 		y: Q.sheets['my_sheepfold'].tileW/2
 	}));
+
+	stage.insert(new Q.Wolf({ x: tiles.p.w/2, y: tiles.p.h/2 }));
+
+	stage.insert(new Q.Wolf({ x: tiles.p.w/4, y: tiles.p.h - (player.p.cy + tiles.p.tileH) }));
+
+	stage.insert(new Q.Wolf({ x: tiles.p.w - Q.sheets['my_sheepfold'].tileW * 2, y: 0, vy: 100 }));
 });
 
 Q.scene('endGame', function (stage) {
@@ -125,7 +195,7 @@ Q.scene('endGame', function (stage) {
 			align: 'center',
 			label: stage.options.label
 		}));
-		
+
 		button_y += Math.ceil(container.children[container.children.length - 1].p.h) + 10;
 	}
 
